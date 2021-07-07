b62a7d4a06a530144d5b3ce49f8f3ed379fb22eb
fudorec <5486391+fudorec@users.noreply.github.com>
2021-07-07 12:38:18 +0800

    fix rewarder

--- contracts/SwapRewarder.sol
+++ contracts/SwapRewarder.sol
@@ -7,24 +7,49 @@ import {ISwapRewarder} from "./interfaces/ISwapRewarder.sol";
 
 contract SwapRewarder is ISwapRewarder {
     IERC20 public immutable dcs;
+    address public immutable owner;
+    address public immutable minter;
     uint256 public mintRewardAmount = 2000 ether;
     uint256 public burnRewardAmount = 100 ether;
 
-    constructor(IERC20 _dcs) public {
-        dcs = _dcs;
+    event RewarderAbort(address indexed to, uint256 amount);
+
+    modifier onlyMinter() {
+        require(minter == msg.sender, "require system role");
+        _;
     }
 
-    function mintReward(address to, uint256) external override {
+    constructor(IERC20 _dcs, address _minter) public {
+        dcs = _dcs;
+        minter = _minter;
+        owner = msg.sender;
+    }
+
+    function mintReward(address to, uint256) external override onlyMinter {
+        if (mintRewardAmount == 0) return;
+
         // amount not used right now
         dcs.transfer(to, mintRewardAmount);
 
         emit SwapRewarded(to, mintRewardAmount, true);
     }
 
-    function burnReward(address to, uint256) external override {
+    function burnReward(address to, uint256) external override onlyMinter {
+        if (burnRewardAmount == 0) return;
+
         // amount not used right now
         dcs.transfer(to, burnRewardAmount);
 
         emit SwapRewarded(to, burnRewardAmount, false);
+    }
+
+    function abort() external {
+        require(owner == msg.sender, "only owner");
+
+        uint256 balance = dcs.balanceOf(address(this));
+
+        dcs.transfer(msg.sender, balance);
+
+        emit RewarderAbort(msg.sender, balance);
     }
 }
