4523aea4da2762ae8c4769f8126e9663972696c2
Zhaohua <chenzhaohua@pku.edu.cn>
2021-08-11 18:33:14 +0800

    add: airdrop

--- contracts/governance/Airdrop.sol
+++ contracts/governance/Airdrop.sol
@@ -6,22 +6,26 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "../interfaces/IStakingUnlock.sol";
+import "hardhat/console.sol";
 
-contract Airdrop is ReentrancyGuard {
+contract Airdrop is ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
 
-    bytes32 public immutable merkleRoot;
+    bytes32 public merkleRoot;
 
     IStakingUnlock public immutable stakingUnlock;
     IERC20 public immutable rewardToken;
     uint256 public immutable deadline;
-    address public immutable owner;
-    mapping(address => bool) public claimed;
+    // address public immutable owner;
+    mapping(uint256 => bool) public claimed;
 
-    event Claim(address user, uint256 amount);
-    event Refund(address owner, uint256 amount);
+    event Claim(address user, uint256 index, uint256 amount);
+    event Refund(uint256 amount);
+    event UpdateMerkleRoot(bytes32 newMerkleRoot);
 
     constructor(
         IStakingUnlock _stakingUnlock,
@@ -29,14 +33,20 @@ contract Airdrop is ReentrancyGuard {
         bytes32 _merkleRoot,
         uint256 _deadline
     ) {
-        owner = msg.sender;
+        // owner = msg.sender;
         stakingUnlock = _stakingUnlock;
         rewardToken = _rewardToken;
         merkleRoot = _merkleRoot;
         deadline = _deadline;
     }
 
-    function claim() external nonReentrant returns (uint256 claimable) {
+    function updateMerkleRoot(bytes32 newMerkleRoot) external onlyOwner {
+        merkleRoot = newMerkleRoot;
+        emit UpdateMerkleRoot(newMerkleRoot);
+    }
+
+    /*
+    function claim() external nonReentrant returns (uint256 claimable) { // user: from airdrop to stakingUnlock
         require(block.timestamp <= deadline, "only before deadline");
         require(!claimed[msg.sender], "already claimed");
 
@@ -46,22 +56,54 @@ contract Airdrop is ReentrancyGuard {
 
         emit Claim(msg.sender, claimable);
     }
+    */
+    function claim(
+        uint256 index,
+        uint256 amount,
+        bytes32[] calldata merkleProof
+    ) external nonReentrant returns (bool flag) {
+        // user: from airdrop to stakingUnlock
+        require(block.timestamp <= deadline, "only before deadline");
+        require(!claimed[index], "already claimed");
 
-    function refund() external nonReentrant returns (uint256 amount) {
-        require(msg.sender == owner, "only owner");
+        claimed[index] = true;
+
+        flag = _claimRewards(msg.sender, index, amount, merkleProof);
+        require(flag, "wrong Merkle proof");
+
+        _stakeUnlock(msg.sender, amount);
+
+        emit Claim(msg.sender, index, amount);
+    }
+
+    function refund() external nonReentrant onlyOwner returns (uint256 amount) {
+        // owner: refund after deadline
+        // require(msg.sender == owner, "only owner");
         require(block.timestamp > deadline, "only after deadline");
 
         amount = rewardToken.balanceOf(address(this));
-        rewardToken.safeTransfer(owner, amount);
+        rewardToken.safeTransfer(owner(), amount);
 
-        emit Refund(owner, amount);
+        emit Refund(amount);
     }
 
+    function _claimRewards(
+        address user,
+        uint256 index,
+        uint256 amount,
+        bytes32[] calldata merkleProof
+    ) private view returns (bool flag) {
+        bytes32 leaf = keccak256(abi.encodePacked(index, user, amount));
+        flag = MerkleProof.verify(merkleProof, merkleRoot, leaf);
+        // console.logBytes32(leaf);
+    }
+
+    /*
     function _claimRewards(address user) private returns (uint256 amount) {
         amount = 1600e18;
         claimed[user] = true;
     }
-
+    */
     function _stakeUnlock(address user, uint256 amount) private {
         rewardToken.approve(address(stakingUnlock), amount);
         stakingUnlock.depositLocked(user, amount);
