121618f7dccc8aa0dcb34806c1b06d08233a4bf7
daleczh <32120796+daleczh@users.noreply.github.com>
2021-08-13 16:21:33 +0800

    Dev czh airdrop (#84)
    * add: airdrop
    
    * fix check
    
    * fix
    
    Co-authored-by: fudorec <5486391+fudorec@users.noreply.github.com>

--- contracts/governance/Airdrop.sol
+++ contracts/governance/Airdrop.sol
@@ -6,22 +6,24 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "../interfaces/IStakingUnlock.sol";
 
-contract Airdrop is ReentrancyGuard {
+contract Airdrop is ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
 
-    bytes32 public immutable merkleRoot;
+    bytes32 public merkleRoot;
 
     IStakingUnlock public immutable stakingUnlock;
     IERC20 public immutable rewardToken;
     uint256 public immutable deadline;
-    address public immutable owner;
-    mapping(address => bool) public claimed;
+    mapping(uint256 => bool) public claimed;
 
-    event Claim(address user, uint256 amount);
-    event Refund(address owner, uint256 amount);
+    event Claim(address user, uint256 index, uint256 amount);
+    event Refund(uint256 amount);
+    event UpdateMerkleRoot(bytes32 newMerkleRoot);
 
     constructor(
         IStakingUnlock _stakingUnlock,
@@ -29,37 +31,52 @@ contract Airdrop is ReentrancyGuard {
         bytes32 _merkleRoot,
         uint256 _deadline
     ) {
-        owner = msg.sender;
         stakingUnlock = _stakingUnlock;
         rewardToken = _rewardToken;
         merkleRoot = _merkleRoot;
         deadline = _deadline;
     }
 
-    function claim() external nonReentrant returns (uint256 claimable) {
-        require(block.timestamp <= deadline, "only before deadline");
-        require(!claimed[msg.sender], "already claimed");
-
-        claimable = _claimRewards(msg.sender);
-
-        _stakeUnlock(msg.sender, claimable);
-
-        emit Claim(msg.sender, claimable);
+    function updateMerkleRoot(bytes32 newMerkleRoot) external onlyOwner {
+        merkleRoot = newMerkleRoot;
+        emit UpdateMerkleRoot(newMerkleRoot);
     }
 
-    function refund() external nonReentrant returns (uint256 amount) {
-        require(msg.sender == owner, "only owner");
+    function claim(
+        uint256 index,
+        uint256 amount,
+        bytes32[] calldata merkleProof
+    ) external nonReentrant returns (bool flag) {
+        require(block.timestamp <= deadline, "only before deadline");
+        require(!claimed[index], "already claimed");
+
+        claimed[index] = true;
+
+        flag = _claimRewards(msg.sender, index, amount, merkleProof);
+        require(flag, "wrong Merkle proof");
+
+        _stakeUnlock(msg.sender, amount);
+
+        emit Claim(msg.sender, index, amount);
+    }
+
+    function refund() external nonReentrant onlyOwner returns (uint256 amount) {
         require(block.timestamp > deadline, "only after deadline");
 
         amount = rewardToken.balanceOf(address(this));
-        rewardToken.safeTransfer(owner, amount);
+        rewardToken.safeTransfer(owner(), amount);
 
-        emit Refund(owner, amount);
+        emit Refund(amount);
     }
 
-    function _claimRewards(address user) private returns (uint256 amount) {
-        amount = 1600e18;
-        claimed[user] = true;
+    function _claimRewards(
+        address user,
+        uint256 index,
+        uint256 amount,
+        bytes32[] calldata merkleProof
+    ) private view returns (bool flag) {
+        bytes32 leaf = keccak256(abi.encodePacked(index, user, amount));
+        flag = MerkleProof.verify(merkleProof, merkleRoot, leaf);
     }
 
     function _stakeUnlock(address user, uint256 amount) private {
