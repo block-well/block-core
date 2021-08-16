55af07cf97af34a75a6e58349662c74b355d4ac5
gougougogol <44010831+gougougogol@users.noreply.github.com>
2021-08-16 17:50:58 +0800

    update liquidation (#86)
    * create treasury contract
    
    * update treasury contract and test case
    
    * fix
    
    * Liquidation Contract
    
    * update liquidation
    
    * fix
    
    Co-authored-by: wjj <wjj@wjjdeMacBook-Pro.local>

---
+++ contracts/governance/Liquidation.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+import {SATS} from "../system/SATS.sol";
+import {IBtcRater} from "../interfaces/IBtcRater.sol";
+
+contract Liquidation is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    using SafeERC20 for SATS;
+
+    SATS public immutable sats;
+    IBtcRater public immutable btcRater;
+    address public immutable keeperRegistryAddress;
+    uint256 public immutable startTimestamp;
+    uint256 public immutable duration;
+    uint256 private constant PRECISE_UNIT = 1e18;
+
+    event AssetAuctioned(
+        address operator,
+        address asset,
+        uint256 amount,
+        uint256 discountSatsAmount
+    );
+
+    constructor(
+        address _sats,
+        address _btcRater,
+        address _registryAddr,
+        uint256 _startTimestamp,
+        uint256 _duration
+    ) {
+        sats = SATS(_sats);
+        btcRater = IBtcRater(_btcRater);
+        keeperRegistryAddress = _registryAddr;
+        startTimestamp = _startTimestamp;
+        duration = _duration; //20 days, 20*24*3600=1728000
+    }
+
+    function discountPrice(uint256 timestamp) external view returns (uint256) {
+        return _discountPrice(timestamp);
+    }
+
+    function _discountPrice(uint256 timestamp) private view returns (uint256) {
+        if (timestamp <= startTimestamp) return PRECISE_UNIT;
+        return PRECISE_UNIT.sub((timestamp.sub(startTimestamp)).mul(PRECISE_UNIT).div(duration));
+    }
+
+    function calcDiscountSatsAmount(address asset, uint256 amount) public view returns (uint256) {
+        uint256 normalizedAmount = btcRater.calcAmountInWei(asset, amount);
+        uint256 satsAmount = btcRater.calcOrigAmount(address(sats), normalizedAmount);
+        uint256 price = _discountPrice(block.timestamp);
+        uint256 discountSatsAmount = satsAmount.mul(price).div(PRECISE_UNIT);
+        return discountSatsAmount;
+    }
+
+    function assetAuction(address asset, uint256 amount) external {
+        require(block.timestamp >= startTimestamp, "auction not start");
+        uint256 discountSatsAmount = calcDiscountSatsAmount(asset, amount);
+        sats.safeTransferFrom(msg.sender, keeperRegistryAddress, discountSatsAmount);
+        IERC20(asset).safeTransfer(msg.sender, amount);
+        emit AssetAuctioned(msg.sender, asset, amount, discountSatsAmount);
+    }
+}
---
+++ test/liquidation.ts
@@ -0,0 +1,191 @@
+import { expect } from "chai";
+import { BigNumber, ethers, Wallet } from "ethers";
+import { deployments, waffle } from "hardhat";
+import { BtcRater, ERC20, KeeperRegistry, Liquidation } from "../build/typechain";
+import { advanceTimeAndBlock, currentTime, DAY } from "./helper";
+
+const { parseEther, parseUnits } = ethers.utils;
+const parseBtc = (value: string) => parseUnits(value, 8);
+const parseBtcInSats = (value: string) => parseUnits(value, 18);
+const PRECISE_UNIT = parseUnits("1", 18);
+
+const setupFixture = deployments.createFixture(async ({ ethers, deployments }) => {
+    await deployments.fixture([]);
+
+    const [deployer, ...users] = waffle.provider.getWallets();
+
+    await deployments.deploy("MockWBTC", {
+        from: deployer.address,
+    });
+    const wbtc = (await ethers.getContract("MockWBTC")) as ERC20;
+
+    await deployments.deploy("MockHBTC", {
+        contract: "MockERC20",
+        from: deployer.address,
+    });
+    const hbtc = (await ethers.getContract("MockHBTC")) as ERC20;
+
+    await deployments.deploy("SATS", {
+        from: deployer.address,
+    });
+    await deployments.execute(
+        "SATS",
+        { from: deployer.address },
+        "grantRole",
+        ethers.utils.id("MINTER_ROLE"),
+        deployer.address
+    );
+    const sats = (await ethers.getContract("SATS")) as ERC20;
+
+    await deployments.deploy("BtcRater", {
+        from: deployer.address,
+        args: [
+            [wbtc.address, hbtc.address, sats.address],
+            [1, 1, 1e8],
+        ],
+    });
+    const rater = (await ethers.getContract("BtcRater")) as BtcRater;
+
+    await deployments.deploy("KeeperRegistry", {
+        from: deployer.address,
+        args: [[wbtc.address, hbtc.address], sats.address, rater.address],
+    });
+    const registry = (await ethers.getContract("KeeperRegistry")) as KeeperRegistry;
+
+    await deployments.deploy("Liquidation", {
+        from: deployer.address,
+        args: [sats.address, rater.address, registry.address, (await currentTime()) + DAY, 1728000],
+    });
+    const liquidation = (await ethers.getContract("Liquidation")) as Liquidation;
+
+    for (const user of users) {
+        await wbtc.mint(user.address, parseBtc("100"));
+        await sats.connect(deployer).mint(user.address, parseBtcInSats("100"));
+
+        await wbtc.connect(user).approve(registry.address, parseBtc("100"));
+        await sats.connect(user).approve(liquidation.address, parseBtcInSats("100"));
+    }
+
+    return { deployer, users, wbtc, sats, registry, liquidation };
+});
+
+describe("Liquidation", function () {
+    let deployer: Wallet;
+    let users: Wallet[];
+    let wbtc: ERC20;
+    let sats: ERC20;
+    let registry: KeeperRegistry;
+    let liquidation: Liquidation;
+
+    beforeEach(async function () {
+        ({ deployer, users, wbtc, sats, registry, liquidation } = await setupFixture());
+    });
+
+    describe("punishKeeper() & confiscate() & assetAuction()", function () {
+        it("can not confiscate if liquidation not up yet", async function () {
+            await expect(registry.confiscate([wbtc.address])).to.revertedWith(
+                "liquidation not up yet"
+            );
+        });
+
+        it("should punish keeper, confiscate asset and auction", async function () {
+            /* --- punish keeper --- */
+            await registry.connect(users[0]).addKeeper(wbtc.address, parseBtc("10"));
+
+            const collateral = parseEther("10");
+            let userData = await registry.getKeeper(users[0].address);
+            expect(userData.asset).to.equal(wbtc.address);
+            expect(userData.amount).to.equal(collateral);
+
+            expect(await registry.confiscations(wbtc.address)).to.equal(0);
+
+            await expect(registry.connect(deployer).punishKeeper([users[0].address]))
+                .to.emit(registry, "KeeperPunished")
+                .withArgs(users[0].address, wbtc.address, collateral);
+
+            userData = await registry.getKeeper(users[0].address);
+            expect(userData.amount).to.equal(0);
+
+            expect(await registry.confiscations(wbtc.address)).to.equal(collateral);
+
+            /* --- update liquidation contract address --- */
+            await registry.connect(deployer).updateLiquidation(liquidation.address);
+            expect(await registry.liquidation()).to.equal(liquidation.address);
+
+            /* --- confiscate asset to liquidation contract --- */
+            expect(await wbtc.balanceOf(registry.address)).to.equal(parseBtc("10"));
+            expect(await wbtc.balanceOf(liquidation.address)).to.equal(0);
+
+            await expect(registry.confiscate([wbtc.address]))
+                .to.emit(registry, "Confiscated")
+                .withArgs(liquidation.address, wbtc.address, collateral);
+
+            expect(await wbtc.balanceOf(registry.address)).to.equal(0);
+            expect(await wbtc.balanceOf(liquidation.address)).to.equal(parseBtc("10"));
+
+            /* --- asset auction --- */
+            //contract wbtc & sats balances now
+            const origLiquidationWbtc = await wbtc.balanceOf(liquidation.address);
+            const origRegistrySats = await sats.balanceOf(registry.address);
+            expect(origLiquidationWbtc).to.equal(parseBtc("10"));
+            expect(origRegistrySats).to.equal(0);
+
+            //users[1] wbtc & sats balances now
+            const origUserWbtc = await wbtc.balanceOf(users[1].address);
+            const origUserSats = await sats.balanceOf(users[1].address);
+            expect(origUserWbtc).to.equal(parseBtc("100"));
+            expect(origUserSats).to.equal(parseBtcInSats("100"));
+
+            //different time to auction
+            const wbtcAmount = parseBtc("10");
+
+            //1. auction now
+            let timestamp = await currentTime();
+            let price = await liquidation.discountPrice(timestamp);
+            expect(price).to.equal(BigNumber.from("1").mul(PRECISE_UNIT));
+
+            let discountSatsAmount = await liquidation.calcDiscountSatsAmount(
+                wbtc.address,
+                wbtcAmount
+            );
+            expect(discountSatsAmount).to.equal(parseBtcInSats("10"));
+
+            await expect(
+                liquidation.connect(users[1]).assetAuction(wbtc.address, wbtcAmount)
+            ).to.revertedWith("auction not start");
+
+            //2. auction after one day
+            await advanceTimeAndBlock(DAY);
+
+            timestamp = await currentTime();
+            price = await liquidation.discountPrice(timestamp);
+            const duration = BigNumber.from(timestamp).sub(await liquidation.startTimestamp());
+            expect(price).to.equal(
+                PRECISE_UNIT.sub(duration.mul(PRECISE_UNIT).div(await liquidation.duration()))
+            );
+
+            discountSatsAmount = await liquidation.calcDiscountSatsAmount(wbtc.address, wbtcAmount);
+            expect(discountSatsAmount).to.equal(parseBtcInSats("10").mul(price).div(PRECISE_UNIT));
+
+            const tx = await liquidation.connect(users[1]).assetAuction(wbtc.address, wbtcAmount);
+            discountSatsAmount = await liquidation.calcDiscountSatsAmount(wbtc.address, wbtcAmount);
+            await expect(tx)
+                .to.emit(liquidation, "AssetAuctioned")
+                .withArgs(users[1].address, wbtc.address, wbtcAmount, discountSatsAmount);
+
+            //contract wbtc & sats balances now
+            expect(await wbtc.balanceOf(liquidation.address)).to.equal(
+                origLiquidationWbtc.sub(wbtcAmount)
+            );
+            expect(await sats.balanceOf(registry.address)).to.equal(
+                origRegistrySats.add(discountSatsAmount)
+            );
+
+            //users[1] wbtc & sats balances now
+            expect(await wbtc.balanceOf(users[1].address)).to.equal(origUserWbtc.add(wbtcAmount));
+            expect(await sats.balanceOf(users[1].address)).to.equal(
+                origUserSats.sub(discountSatsAmount)
+            );
+        });
+    });
+});
