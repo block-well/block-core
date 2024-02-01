64503aa051d1bb7379788ca9ce57ff88ed7ad078
mingming.tang <mingmingtang@aliyun.com>
2024-02-01 20:52:53 +0800

    add proxy

--- scripts/upgrade_swap_fee_dcs_to_v2.js
+++ scripts/upgrade_swap_fee_dcs_to_v2.js
@@ -3,7 +3,7 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
   // Deploying
   const SwapFeeDcsV2 = await ethers.getContractFactory("SwapFeeDcsV2");
-  const instance = await upgrades.upgradeProxy("0xFf013054037B6B139184150e9651E31c7B8aA6B5", SwapFeeDcsV2);
+  const instance = await upgrades.upgradeProxy("0x053fA738b724d8d5d45856136FAe81F298b504a8", SwapFeeDcsV2);
   console.log('SwapFeeDcs upgraded');
   const version = await instance.getVersion();
   console.log("Version: ", version)
--- scripts/upgrade_swap_fee_sats_to_v2.js
+++ scripts/upgrade_swap_fee_sats_to_v2.js
@@ -3,7 +3,7 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
   // Deploying
   const SwapFeeSatsV2 = await ethers.getContractFactory("SwapFeeSatsV2");
-  const instance = await upgrades.upgradeProxy("0xb666627fAcBD41bDCAD525E378F264D3a2F8836c", SwapFeeSatsV2);
+  const instance = await upgrades.upgradeProxy("0x99aB38e76E133f14CFaEFaA848Cb10d86389D5a5", SwapFeeSatsV2);
   console.log('SwapFeeSats upgraded');
   const version = await instance.getVersion();
   console.log("Version: ", version)
